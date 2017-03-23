2db1a7db4f70a7fb98b5594b685d405d76415072
paokodo <pao55lo@gmail.com>
2017-03-23 23:47:55 +0700

    add sample php funcion gobal top

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 function decryptCookie($value){
+  global $key;
 	echo $value;
 	// global $conn,$key,$iv,$aes256Key;
 	if(!$value){return false;}
@@ -12,7 +13,7 @@ function decryptCookie($value){
 	$decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $crypttext, MCRYPT_MODE_ECB, $iv);
 	// return trim($decrypttext);
 	// return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $aes256Key, base64_decode($value), MCRYPT_MODE_CBC, $iv), "\0\3");
-	global $key;
+
 	srand((double) microtime() * 1000000);
 	$iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_RAND);
 	$aes256Key = hash("SHA256", $key, true);
